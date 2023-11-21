4956e094327ab4924eccab3c726a8f523df53365
Falbanese5 <falbanese5@gmail.com>
2023-11-20 21:09:55 -0500

    Assignment 20 and 21

--- exercises/21-transforming-dom/transforming-dom.js
+++ exercises/21-transforming-dom/transforming-dom.js
@@ -5,4 +5,17 @@
  */
 (function () {
   // Put your answers in here
+  //let img = document.querySelector("img");
+  document.querySelector("img").src = "https://media.giphy.com/media/3oKIPnAiaMCws8nOsE/giphy.gif";
+  //document.querySelector(".alert alert-warning").target = "https://developer.mozilla.org/en-US/docs/Web/JavaScript";
+  let javaLink = (document.querySelector("#javaLink"));
+  javaLink.href = "https://developer.mozilla.org/en-US/docs/Web/JavaScript";
+  let success = (document.querySelector(".alert-success"));
+  success.textContent = "I am victorious!";
+  let button = (document.querySelector(".btn-primary"));
+  let check = (document.querySelector("#dark"));
+  if (button.classList.contains("btn-primary"))
+  {
+    check.textContent = "✓ blue";
+  }
 })();
